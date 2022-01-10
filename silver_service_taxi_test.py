36d888a6edce5932501391dc9dd7a3f9c6a51fd2
Caleb <jiale.hou@my.jcu.edu.au>
2022-01-10 15:41:40 +0800

    Add tests for silver service taxi test.

--- silver_service_taxi_test.py
+++ silver_service_taxi_test.py
@@ -2,7 +2,12 @@ from silver_service_taxi import SilverServiceTaxi
 
 
 def main():
-    pass
+    test_car = SilverServiceTaxi("test taxi", 20, 2)
+    test_car.drive(18)
+    print(test_car)
+    current_fare = test_car.get_fare()
+    print("current fare: ${}".format(current_fare))
+
 
 if __name__ == '__main__':
     main()
